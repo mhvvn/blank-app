88515ae7b19956655615bf7d9c87d77100fbeab4
Muh Veven <mhmmdveven@gmail.com>
2025-12-16 15:44:29 +0700

    Enhance Streamlit app with data selection and charts
    Added data selection and visualization features.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title("🎈 This My First App")
 st.write(
@@ -11,8 +13,6 @@ st.write(
 Here's our first attempt at using data to create a table:
 """
 
-import streamlit as st
-import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
@@ -21,23 +21,34 @@ df = pd.DataFrame({
 df
 
 
-import streamlit as st
-import numpy as np
-import pandas as pd
-
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
 
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-import streamlit as st
-import numpy as np
-import pandas as pd
+st.line_chart(chart_data)
+
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
